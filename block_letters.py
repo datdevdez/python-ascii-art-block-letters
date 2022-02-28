8e63439da605a15f2e1c4988ebea47db111211d8
Dezmond Williams <93957972+datdevdez@users.noreply.github.com>
2022-02-28 13:18:11 -0500

    Create block_letters.py

---
+++ block_letters.py
@@ -0,0 +1,22 @@
+##Name: Dezmond Williams
+##Fun Fact: I like penguins
+
+print('''
+D D D D
+D       D
+D       D
+D       D
+D       D
+D       D
+D D D D
+''')
+
+print('''
+W      W
+W      W
+W      W
+W   W  W
+W W  W W
+W W  W W
+W      W
+''')
